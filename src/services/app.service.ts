2176da402f4280ef38f4cd623f2cab0dd89c2c52
jiingler <jinglelin82@gmail.com>
2023-01-10 00:55:23 +0900

    change ' -> "(ts)

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -2,33 +2,33 @@ export class AppService {
   baseUrl = process.env.REACT_APP_API_URL as string;
 
   public async get<T>(url: string, criteria: any = null): Promise<T> {
-    let query = '';
+    let query = "";
     if (criteria) {
       Object.keys(criteria).forEach((key, index) => {
         const str = `${key}=${criteria[key]}`;
         query += str;
         if (index <= Object.keys(criteria).length - 1) {
-          query += '&';
+          query += "&";
         }
       });
     }
 
-    const response = await fetch(`${this.baseUrl}/${url}${criteria ? `?${query}` : ''}`);
+    const response = await fetch(`${this.baseUrl}/${url}${criteria ? `?${query}` : ""}`);
     return await response.json();
   }
 
   public async post<T>(url: string, model: any): Promise<T> {
-    const response = await fetch(`${this.baseUrl}/${url}`, { method: 'POST', body: model });
+    const response = await fetch(`${this.baseUrl}/${url}`, { method: "POST", body: model });
     return await response.json();
   }
 
   public async put<T>(url: string, model: any): Promise<T> {
-    const response = await fetch(`${this.baseUrl}/${url}`, { method: 'PUT', body: model });
+    const response = await fetch(`${this.baseUrl}/${url}`, { method: "PUT", body: model });
     return await response.json();
   }
 
   public async delete<T>(url: string): Promise<T> {
-    const response = await fetch(`${this.baseUrl}/${url}`, { method: 'DELETE' });
+    const response = await fetch(`${this.baseUrl}/${url}`, { method: "DELETE" });
     return await response.json();
   }
 }
