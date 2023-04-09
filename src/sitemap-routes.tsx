361c3d26030394a38c8eb1cd37b27d2930f78e16
jiingler <jinglelin82@gmail.com>
2023-04-09 22:37:22 +0900

    update sitemap

--- src/sitemap-routes.tsx
+++ src/sitemap-routes.tsx
@@ -5,9 +5,9 @@ export default (
   <Route>
     <Route path="/"></Route>
     <Route path="/news"></Route>
-    <Route path="/news/:id"></Route>
     <Route path="/doctors"></Route>
     <Route path="/clinics"></Route>
     <Route path="/services"></Route>
+    <Route path="/articles"></Route>
   </Route>
 );
